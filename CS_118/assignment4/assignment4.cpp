0c8372b0a92d54b9f0ceadf1f36bb155b33cb7e5
Tino Lei'a <valentinoleia@yahoo.com>
2020-02-16 06:29:10 -0800

    finished assignment4

--- CS_118/assignment4/assignment4.cpp
+++ CS_118/assignment4/assignment4.cpp
@@ -11,17 +11,70 @@ Description: Write C/C++ code to extract exponent,
 #include <stdio.h>
 #include <stdint.h>
 
-using namespace std;
 
 	// global variables
-	uint32_t original, exponent, mantissa;
+	const float decimal = -7.75;				// 0111.0100 1101
+	uint32_t* ptr = (uint32_t*)&decimal;
+
+	// function prototypes
+	void extractSignBit(uint32_t*);
+	void extractExponentBits(uint32_t*);
+	void extractMantissa(uint32_t*);
+
+using namespace std;
 
 	// driver code
 	int main(){
 
-		printf("Here is a float: %f\n\n", a);
-		cout << "float pointer: " << *p << endl;
-//		printf("float pointer: %f\n", p);
-
+		printf("float: %x\n", *ptr);
+		extractSignBit(ptr);
+		extractExponentBits(ptr);
+		extractMantissa(ptr);
+		
 		return 0;
 	}
+
+
+		/*********************************************************
+		* initialize an unsigned 32 bit integer to the value of  *
+		* the pointer p. then print that value to the console    *
+		* in hexadecimal. then shift the bits 31 bits to the     *
+		* right starting at the signbit. then print that sign    *
+		*bit to the console.                                     * 
+		*********************************************************/
+		void extractSignBit(uint32_t* p){
+
+			uint32_t signBit = *p;
+			signBit >>= 31;
+			printf("sign bit: %x\n", signBit);
+		}
+
+		/*********************************************************
+		* initialize an unsigned 32 bit integer to the value of  *
+		* the pointer p. then print that value to the console    *
+		* in hexadecimal. then shift the bits 31 bits to the     *
+		* right starting at the signbit. then print that sign    *
+		*bit to the console.                                     * 
+		*********************************************************/
+		void extractExponentBits(uint32_t* p){
+		
+			uint32_t exponentBits = *p;
+			exponentBits <<= 1;
+			exponentBits >>= 24;		
+			printf("exponent bit: %x\n", exponentBits);
+		}	
+
+		/*********************************************************
+		* initialize an unsigned 32 bit integer to the value of  *
+		* the pointer p. then print that value to the console    *
+		* in hexadecimal. then shift the bits 31 bits to the     *
+		* right starting at the signbit. then print that sign    *
+		*bit to the console.                                     * 
+		*********************************************************/
+		void extractMantissa(uint32_t* p){
+		
+			uint32_t mantissa = *p;
+			mantissa <<= 9;
+			mantissa >>= 9;
+			printf("mantissa bit: %x\n", mantissa);
+		}
--- random_files/test.cpp
+++ random_files/test.cpp
@@ -3,22 +3,53 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdint.h>
+#include <cmath>
 
 using namespace std;
 
-int main(){
-	/*
-	uint32_t x = 651;
-	uint32_t a = 0x000000FF;    // 15              = 0x000000FF = 0000 0000 0000 0000 0000 0000 1111 1111
-	uint32_t f = 0xFFFFFFFF;    // 4,294,967,295   = 0xFFFFFFFF = 1111 1111 1111 1111 1111 1111 1111 1111
+// from 31 to 23 is the base
+    uint32_t b = 0x80000000;    		// 01              = 0x00000001 = 0000 0000 0000 0000 0000 0000 0000 0001
+	const float decimal = 12.8; 		// 7.75            = 0x00000000 = 0000 0000 0000 0000 0000 0111.0100 1011
+    int countHighBits(const float);
 
-	printf("uint32 x = %x\n", x);
-	printf("uint32 a = %x\n", a);
-	printf("uint32 f = %x\n", f);
-*/
 
-	
-	const float decimal = 7.75; // 7.75            = 0x00000000 = 0000 0000 0000 0000 0000 0111.0100 1011
-	printf("float: %d\n", decimal);
-	return 0;
-}
+	int main(){
+		/*
+		uint32_t x = 651;
+		uint32_t a = 0x000000FF;    // 15              = 0x000000FF = 0000 0000 0000 0000 0000 0000 1111 1111
+		uint32_t f = 0xFFFFFFFF;    // 4,294,967,295   = 0xFFFFFFFF = 1111 1111 1111 1111 1111 1111 1111 1111
+
+		printf("uint32 x = %x\n", x);
+		printf("uint32 a = %x\n", a);
+		printf("uint32 f = %x\n", f);
+	*/
+
+		if(isnan(decimal) == false){
+			cout << "false\n";
+		}
+
+		cout << "high bits: " << countHighBits(decimal) << endl;
+		
+		printf("float: %f\n", decimal);
+		return 0;
+	}
+
+	/************************************************************
+    * countHighBits accepts one uint32_t argument and will      *
+    * check if each bit is high or not then sum the high bits   *
+    * in a local variable and return that sum.
+    ************************************************************/
+    int countHighBits(const float num){
+
+      int count = 0;
+      for (int i = 0; i < 32; ++i){
+
+        if(num & b){
+
+          count++;
+        }
+
+        b >>= 1;
+      }
+      return count;
+    }
