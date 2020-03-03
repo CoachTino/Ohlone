f750916ca3df0032403330005b730c27bf23ccf8
Tino Lei'a <valentinoleia@yahoo.com>
2020-03-02 19:46:55 -0800

    lab3

---
+++ CS_118/lab/lab3.asm
@@ -0,0 +1,13 @@
+extern printf					; the C function to be called
+global myprint				; the standard gcc entry point
+
+SECTION .text
+	
+myprint:
+
+	push	rbp					; set up stack frame
+	call 	printf				; call C function
+	pop		rbp					; restore stack
+
+	mov		rax, 0				; normal, no error, return value
+	ret 						; return
---
+++ CS_118/lab/lab3.c
@@ -0,0 +1,10 @@
+// c program to call assembly file
+
+void myprint(const char*,int,const char*);
+
+void main(){
+
+	myprint("Hello,  %d,%s\n", 12, " it worked!");
+
+	//return 0;
+}
