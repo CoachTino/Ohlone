e3a464586f950febf17553cd812ec79839f5ec1f
Tino <valentinoleia@yahoo.com>
2020-10-27 21:30:18 -0700

    need to fix a bug
    user can enter name and palindrome without actually typing anything in + fix print out to user when reading file into memory

--- CS_124/lab_reports/palindromes/src/palindromes.cpp
+++ CS_124/lab_reports/palindromes/src/palindromes.cpp
@@ -6,11 +6,6 @@
 
 #include <iostream>
 #include "userInterface.h"
-// #include "fileInputAndOutput.h"
-// #include "linkedList.h"
-// #include "ListNode.h"
-// #include "dataValidation.h"
-
 
 int main(){
 	
